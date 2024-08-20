661483bdb089d5792048095f5d6c7764f7e38362
PJCreation <2320053330@qq.com>
2024-08-20 19:04:41 +0000

    Site updated: 2024-08-20 19:04:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/20/hello-world/","2024/08/20/test/"];function toRandomPost(){
+var posts=["2024.08/AI大模型的本地部署——安装open-webui/","2024.08/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
