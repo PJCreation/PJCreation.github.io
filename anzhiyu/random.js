814dddd5ef643eaa2a7f4c7f5585a49cb054ac7a
PJCreation <2320053330@qq.com>
2024-08-22 16:12:16 +0000

    Site updated: 2024-08-22 16:12:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024.08/AI大模型的本地部署——安装open-webui/","2024.08/hello-world/"];function toRandomPost(){
+var posts=["2024.08/AI大模型的本地部署——安装open-webui/","2024.08/hello-world/","2022.06/Linux-Docker命令/","2024.08/转-Python虚拟环境教程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
